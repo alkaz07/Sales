d2afeee682c44a51e5f4fd5ce22f80f47418f21b
alkaz07 <alkaz07@yandex.ru>
2023-04-05 19:15:33 +0300

    Проверено чтение исходного файла

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        try {
+            Scanner sc = new Scanner(new File("input.txt"));
+            int ammountOfSales = sc.nextInt();
+            String[] product = new String[ammountOfSales];
+            double[] weight = new double[ammountOfSales];
+            int[] price = new int[ammountOfSales];
+            for (int i = 0; i < ammountOfSales; i++) {
+                product[i] = sc.next();
+                weight[i] = sc.nextDouble();
+                price[i] = sc.nextInt();
+            }
+
+            System.out.println(Arrays.toString(product));
+            System.out.println(Arrays.toString(weight));
+            System.out.println(Arrays.toString(price));
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
     }
 }
